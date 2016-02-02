33684fb4aa46fe1d2d0121f4de51451f04f488fd
masaponto <masaponto@gmail.com>
2016-02-02 16:52:03 +0900

    [ADD] init

---
+++ main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+from sklearn import preprocessing
+from sklearn.base import BaseEstimator
+from sklearn.datasets import fetch_mldata
+from sklearn import cross_validation
+from sklearn.datasets import load_svmlight_file
+from sklearn.svm import SVC
+from sklearn.utils import shuffle
+
+from dbm import DBM
+from mlp.three_layer_mlp import TLMLP
+
+from multiprocessing import Pool
+
+
+def argwrapper(args):
+    '''
+    ラッパー関数
+    '''
+    return args[0](*args[1:])
+
+
+def my_cv(estimator, data_set, cv=5):
+    from sklearn.utils import shuffle
+    X, y = shuffle(data_set.data, data_set.target,
+                   random_state=np.random.RandomState())
+
+    n = data_set.data.shape[0]
+    k = n // cv
+    # print(n,cv,k)
+    scores = []
+
+    for index in range(0, n - (n % cv), k):
+        #e = estimator
+        x_test = X[index: index + k]
+        y_test = y[index: index + k]
+
+        x_train1 = X[:index]
+        y_train1 = y[:index]
+
+        x_train2 = X[index + k:]
+        y_train2 = y[index + k:]
+
+        x_train = np.r_[x_train1, x_train2]
+        y_train = np.r_[y_train1, y_train2]
+
+        estimator.fit(x_train, y_train)
+        re = estimator.predict(x_test)
+        score = sum([r == y for r, y in zip(re, y_test)]) / len(y_test)
+        scores.append(score)
+
+    # print(scores)
+    return np.average(scores)
+
+
+def pararell_cv_mlp(data_set, hid_num=100, epochs=1000):
+    p = Pool(8)
+    print('==MLP', 'hid_num', hid_num, 'epochs', epochs)
+    cv = 5
+    mlp = TLMLP(hid_num, epochs)
+    func_args = [(my_cv, mlp, data_set, cv) for i in range(10)]
+    score_list = p.map(argwrapper, func_args)
+    print("Accuracy %0.3f (+/- %0.3f)" %
+          (np.average(score_list), np.std(score_list) * 2))
+
+
+def pararell_cv_dbm(data_set, hid_num=100, epochs=1000):
+    p = Pool(8)
+    print('==DBM', 'hid_num', hid_num, 'epochs', epochs)
+    cv = 5
+    dbm = DBM(hid_num=hid_num, epochs=epochs)
+    func_args = [(my_cv, dbm, data_set, cv) for i in range(10)]
+    score_list = p.map(argwrapper, func_args)
+    print("Accuracy %0.3f (+/- %0.3f)" %
+          (np.average(score_list), np.std(score_list) * 2))
+
+
+def main():
+
+    db_name = 'australian'
+    data_set = fetch_mldata(db_name)
+    data_set.data = preprocessing.scale(data_set.data)
+
+    pararell_cv_mlp(data_set)
+    pararell_cv_dbm(data_set)
+
+
+if __name__ == "__main__":
+    main()
---
+++ mlp/three_layer_mlp.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+from sklearn import preprocessing
+from sklearn.base import BaseEstimator
+from sklearn.datasets import fetch_mldata
+from sklearn import cross_validation
+from sklearn.datasets import load_svmlight_file
+
+
+class TLMLP(BaseEstimator):
+    """
+    Multi Layer Perceptron (Sngle hidden layer)
+
+    """
+
+    def __init__(self, hid_num=10, epochs=1000, r=0.5):
+        """
+        mlp using sigmoid
+        Args:
+        hid_num int : number of hidden neuron
+        out_num int : number of output neuron
+        epochs int: number of epoch
+        r float: learning rate
+        """
+        self.hid_num = hid_num
+        self.epochs = epochs
+        self.r = r
+
+    def _sigmoid(self, x, a=1):
+        """
+        sigmoid function
+        Args:
+        x float
+        Returns:
+        float
+        """
+        return 1 / (1 + np.exp(- a * x))
+
+    def _dsigmoid(self, x, a=1):
+        """
+        diff sigmoid function
+        Args:
+        x float
+        Returns:
+        float
+        """
+        return a * x * (1.0 - x)
+
+    def _add_bias(self, x_vs):
+        """
+        add bias to list
+
+        Args:
+        x_vs [[float]] Array: vec to add bias
+
+        Returns:
+        [float]: added vec
+        """
+
+        return np.c_[x_vs, np.ones(len(x_vs))]
+
+    def _ltov(self, n):
+        """
+        trasform label(integer) to vector (list)
+
+        Args:
+        n (int) : number of class, number of out layer neuron
+        label (int) : label
+
+        Examples:
+        >>> mlp = MLP(10, 3)
+        >>> mlp._ltov(3)(1)
+        [1, 0, 0]
+        >>> mlp._ltov(3)(2)
+        [0, 1, 0]
+        >>> mlp._ltov(3)(3)
+        [0, 0, 1]
+        """
+        def inltov(label):
+            if self.out_num is 1:
+                return 0 if n is -1 else 1
+            else:
+                return [0 if i != label else 1 for i in range(1, n + 1)]
+        return inltov
+
+    def _vtol(self, vec):
+        """
+        tranceform vector (list) to label
+        Args:
+        v: int list, list to transform
+
+        Returns:
+        int : label of classify result
+
+        Exmples:
+        >>> p = MLP(10, 3)
+        >>> p.out_num = 3
+        >>> p._vtol([1, -1, -1])
+        1
+        >>> p._vtol([-1, 1, -1])
+        2
+        >>> p._vtol([-1, -1, 1])
+        3
+        """
+
+        if self.out_num == 1:
+            return 1 if 1 <= round(vec[0], 0) else -1
+        else:
+            v = list(vec)
+            return int(v.index(max(v))) + 1
+
+    def _calc_out(self, w, x):
+        return self._sigmoid(np.dot(w, x))
+
+    def _out_error(self, z, y):
+        return (z - y) * self._dsigmoid(z)
+
+    def _hid_error(self, z, eo):
+        return np.dot(self.wo.T, eo) * self._dsigmoid(z)
+
+    def _w_update(self, w, e, z):
+        e = np.atleast_2d(e)
+        z = np.atleast_2d(z)
+        return w - self.r * np.dot(e.T, z)
+
+    def fit(self, X, y):
+        """
+        学習
+        Args:
+        X [[float]] array : featur vector
+        y [int] array : class labels
+        """
+
+        self.out_num = max(y)
+
+        if not (self.out_num is 1):
+            y = np.array(list(map(self._ltov(self.out_num), y)))
+
+        X = self._add_bias(X)
+
+        np.random.seed()
+        self.wh = np.random.uniform(-1.0, 1.0, (self.hid_num, X.shape[1]))
+        self.wo = np.random.uniform(-1.0, 1.0, (self.out_num, self.hid_num))
+
+        for n in range(self.epochs):
+            for _x, _y in zip(X, y):
+                # forward phase
+                # 中間層の結果
+                zh = self._calc_out(self.wh, _x)
+                # 出力層の結果
+                zo = self._calc_out(self.wo, zh)
+
+                # backward phase
+                # 出力層の誤差
+                eo = self._out_error(zo, _y)
+
+                # 中間層
+                eh = self._hid_error(zh, eo)
+
+                # weight update
+                # 出力層
+                self.wo = self._w_update(self.wo, eo, zh)
+                # 中間層
+                self.wh = self._w_update(self.wh, eh, _x)
+
+    def predict(self, x):
+        """
+        Args:
+        x_vs [[float]] array
+        """
+        x = self._add_bias(x)
+        y = self._calc_out(self.wo, self._calc_out(self.wh, x.T))
+
+        return np.array(list(map(self._vtol, y.T)))
+
+
+def main():
+    #db_names = ['iris', 'australian']
+    db_names = ['australian']
+    #db_names = ['iris']
+    hid_nums = [10, 20, 30, 40]
+
+    for db_name in db_names:
+        print(db_name)
+        # load iris data set
+        data_set = fetch_mldata(db_name)
+        data_set.data = preprocessing.scale(data_set.data)
+
+        mlp = TLMLP(10, 1000)
+        mlp.fit(data_set.data, data_set.target)
+        re = mlp.predict(data_set.data)
+        score = sum([r == y for r, y in zip(re, data_set.target)]
+                    ) / len(data_set.target)
+        print(score)
+
+
+if __name__ == "__main__":
+    #import doctest
+    # doctest.testmod()
+    main()
