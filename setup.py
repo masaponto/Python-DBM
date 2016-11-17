c86b028306867b17203009a321fa17141daaad94
masaponto <masaponto@gmail.com>
2016-11-17 17:32:29 +0900

    [WIP]

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='dbm',
     version='0.2',
-    description='decision boundary maiking using mlp',
+    description='Decision boundary maiking',
     author='masaponto',
     author_email='masaponto@gmail.com',
     url='masaponto.github.io',
     install_requires=['scikit-learn==0.18.1',
                       'scipy==0.18.1', 'numpy==1.11.2'],
-    py_modules=["dbm"],
+    py_modules=['dbm'],
     package_dir={'': 'src'}
 )
