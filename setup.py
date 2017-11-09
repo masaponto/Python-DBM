f9aeaca6ab8e426034a0e4e6de98cf64d39ecb86
masaponto <masaponto@gmail.com>
2017-11-09 17:44:46 +0900

    [Update]

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
     author='masaponto',
     author_email='masaponto@gmail.com',
     url='masaponto.github.io',
-    install_requires=['scikit-learn==0.18.1',
-                      'scipy==0.18.1', 'numpy==1.11.2'],
+    install_requires=['scikit-learn>=0.18.1', 'numpy'],
     py_modules=['svm_dbm'],
     package_dir={'': 'src'}
 )
